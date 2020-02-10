94b259fee5b7ee0a9bbd2f4ce2f9bb68b1d3fabe
moon maria <56239469+kha0213@users.noreply.github.com>
2020-02-10 18:19:20 +0900

    11

--- Ex20_02/src/com/ex02/Bj_1931_회의실배정.java
+++ Ex20_02/src/com/ex02/Bj_1931_회의실배정.java
@@ -1,44 +1,54 @@
 package com.ex02;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.Comparator;
-import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class Bj_1931_회의실배정 {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int N = sc.nextInt();
-		sc.nextLine();
-		int[][] conference = new int[N][2];
-		String[] con = new String[N];
-		String[] conTemp=new String[2];
-		for (int i = 0; i < N; i++) {
-			con[i] = sc.nextLine();
-			conTemp = con[i].split(" ");
-			conference[i][0] = Integer.parseInt(conTemp[0]);
-			conference[i][1] = Integer.parseInt(conTemp[1]);
-		}
+	 public static void main(String[] args) {
+	        try {
 
-		Arrays.sort(conference, new Comparator<int[]>() {
+	            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	            int N = Integer.parseInt(br.readLine());
+	            int[][] conference = new int[N][2];
 
-			@Override
-			public int compare(int[] o1, int[] o2) {
-				return o1[1]-o2[1];
-			}
-		});
-		
-		int cnt=1;
-		int temp=conference[0][1];
-		for(int i=1;i<N;i++) {
-			if(conference[i][0]<temp) {
-				continue;
-			}else {
-				cnt++;
-				temp = conference[i][1];
-			}
-		}
-		
-		System.out.println(cnt);
-		sc.close();
-	}
+	            StringTokenizer st = null;
+	            for (int i = 0; i < N; i++) {
+	                st = new StringTokenizer(br.readLine());
+
+	                for (int j = 0; j < 2; j++) {
+	                    conference[i][j] = Integer.parseInt(st.nextToken());
+	                }
+	            }
+
+	            Arrays.sort(conference, new Comparator<int[]>() {
+
+	                @Override
+	                public int compare(int[] o1, int[] o2) {
+	                    if(o1[1]==o2[1]) {
+	                    	return o1[0]-o2[0];
+	                    }
+	                    
+	                    return o1[1] - o2[1];
+	                }
+	            });
+	            int cnt = 1;
+	            int temp = conference[0][1];
+	            for (int i = 1; i < N; i++) {
+	                if (conference[i][0] < temp) {
+
+	                } else {
+	                    cnt++;
+	                    temp = conference[i][1];
+	                }
+	            }
+	            System.out.println(cnt);
+	            br.close();
+	        } catch (IOException e) {
+	            e.printStackTrace();
+	        }
+	    }
 }
--- Ex20_02/src/com/ex02/Main.java
+++ Ex20_02/src/com/ex02/Main.java
@@ -5,48 +5,50 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.Comparator;
-import java.util.Scanner;
 import java.util.StringTokenizer;
 
 public class Main {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int N = sc.nextInt();
-		sc.nextLine();
-		int[][] conference = new int[N][2];
-		
-		StringTokenizer st = null;
-		for (int i = 0; i < N; i++) {
-			try {
-				st = new StringTokenizer(br.readLine());
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-			for(int j=0;j<2;j++) {
-				conference[i][j]=Integer.parseInt(st.nextToken());
-			}
-		}
+    public static void main(String[] args) {
+        try {
 
-		Arrays.sort(conference, new Comparator<int[]>() {
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            int N = Integer.parseInt(br.readLine());
+            int[][] conference = new int[N][2];
 
-			@Override
-			public int compare(int[] o1, int[] o2) {
-				return o1[1]-o2[1];
-			}
-		});
-		int cnt=1;
-		int temp=conference[0][1];
-		for(int i=1;i<N;i++) {
-			if(conference[i][0]<temp) {
-				
-			}else {
-				cnt++;
-				temp = conference[i][1];
-			}
-		}
-		
-		System.out.println(cnt);
-		sc.close();
-	}
+            StringTokenizer st = null;
+            for (int i = 0; i < N; i++) {
+                st = new StringTokenizer(br.readLine());
+
+                for (int j = 0; j < 2; j++) {
+                    conference[i][j] = Integer.parseInt(st.nextToken());
+                }
+            }
+
+            Arrays.sort(conference, new Comparator<int[]>() {
+
+                @Override
+                public int compare(int[] o1, int[] o2) {
+                    if(o1[1]==o2[1]) {
+                    	return o1[0]-o2[0];
+                    }
+                    
+                    return o1[1] - o2[1];
+                }
+            });
+            int cnt = 1;
+            int temp = conference[0][1];
+            for (int i = 1; i < N; i++) {
+                if (conference[i][0] < temp) {
+
+                } else {
+                    cnt++;
+                    temp = conference[i][1];
+                }
+            }
+            System.out.println(cnt);
+            br.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
